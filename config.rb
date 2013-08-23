9a601e2f8d5262396b17306dd52e80ad843e47ed
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-08-23 19:19:01 +0200

    ga

--- config.rb
+++ config.rb
@@ -99,6 +99,21 @@ helpers do
     <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
   HTML
   end
+
+  def google_analytics_account_id
+    'UA-43451052-1'
+  end
+
+  def google_analytics_js
+    ('<script type="text/javascript">' + <<-JS + '</script>').html_safe
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', '#{google_analytics_account_id}', 'glebm.com');
+      ga('send', 'pageview');
+    JS
+  end
 end
 
 set :css_dir, 'stylesheets'
