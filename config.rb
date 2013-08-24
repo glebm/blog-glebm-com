e43b6f13e7dabc34209331fa9cd4b80df52f734c
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-08-24 18:20:56 +0200

    upd

--- config.rb
+++ config.rb
@@ -9,13 +9,13 @@ activate :blog do |blog|
   # blog.permalink = ":year/:month/:day/:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
+  blog.layout = "blog_article"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".markdown.erb"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
